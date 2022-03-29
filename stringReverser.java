cabb04c8233f9a9b47e6404e69cfe36095478f37
Liam Csiffary <liam.csiffary@stu.ocsb.ca>
2022-03-29 08:57:35 -0400

    comments and made txt file reader

--- stringReverser.java
+++ stringReverser.java
@@ -1,30 +1,82 @@
+/**
+* This code reverses a bunch of string from
+*
+* @author  Liam Csiffary
+* @version 1.0.1
+* @since   2022-03-29
+*/
+
+// import necessary modules
+import java.io.File;
+import java.util.List;
 import java.util.Scanner; 
 
+
+// class
 public class stringReverser {
 
+  // generates the array of inputs from a text file
+  public static String[] ArrayGenerator(File file) throws Exception {
 
+    // defining vars
+    List<String> originalListString = new ArrayList<String>();
+    System.out.println(file);
+    Scanner scStudent = new Scanner(file);
+
+    // pass the path to the file as a parameter
+    while (scStudent.hasNextLine()) {
+      originalListString.add(scStudent.nextLine());
+    }
+
+    // for testing purposes
+    System.out.println(originalListString);
+
+    // create array of length
+    String[] array = new String[originalListString.size()];
+
+    // fill the array
+    for (int i = 0; i < array.length; i++) {
+      array[i] = originalListString.get(i);
+    }
+
+    return array;
+  }
+
+  // reverses the string
   public static String reverser(String originalString) {
 
+    // whilst the oString is not empty keep reruning the function
+    // I like to put it on top because it is the most common outcome
     if (!originalString.equals("")) {
+      // takes the remaining string save the first char and sends it back to the File
+      // saves the first letter for when we reconstruct after the string has been exhausted
       return reverser(originalString.substring(0 + 1)) + originalString.substring(0, 1);
 
     } else  {
-      System.out.println("Finished");
+      // once we've exhausted the string
       return originalString;
-      
+
     }
   }
 
 
   public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("This is the string reverser program!");
-    System.out.println("Please inut a string to be reversed: ");
-    String userString = scanner.nextLine();
 
-    String reversedString = reverser(userString);
-    System.out.println("end");
-    System.out.println(reversedString);
+    // creates the file object
+    File file = new File(
+        "C:/Users/s299776/ICS4U/Unit2/Java/Unit2-01-Java/testCases.txt");
 
+    // gets the array of testCases from the ArrayGenerator
+    String[] testCases = ArrayGenerator(file);
+
+    // goes to next line for beautifying reasons
+    System.out.println("\n");
+
+    // prints the normal string and the reversed string for each index
+    // of the array, also sends it down an extra line so it's easier to read
+    for (int i = 0; i < testCases.length; i++) {
+      System.out.println("Original string: " + testCases[i]);
+      System.out.println("Reversed string: " + reverser(testCases[i]) + "\n");
+    }
   }
 }
